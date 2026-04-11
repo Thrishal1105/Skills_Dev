7c5687378a831d4b4749d38caae80df0e91c1e25
Thrishal1105 <dharshanojthrishal11@gmail.com>
2026-04-12 00:57:14 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
